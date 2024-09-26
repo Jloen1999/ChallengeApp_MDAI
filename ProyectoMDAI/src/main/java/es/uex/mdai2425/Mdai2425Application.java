00e3613dc8a6db7e51d7406856724d367ffbc5ee
alokenveo <alokenveo@alumnos.unex.es>
2024-09-26 20:48:59 +0200

    Habían funciones de paquetes que todavía no se habían creado

--- ProyectoMDAI/src/main/java/es/uex/mdai2425/Mdai2425Application.java
+++ ProyectoMDAI/src/main/java/es/uex/mdai2425/Mdai2425Application.java
@@ -1,29 +1,13 @@
 package es.uex.mdai2425;
 
-import es.uex.mdai2425.services.PersonaService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Mdai2425Application implements CommandLineRunner {
-
-    @Autowired
-    private final PersonaService personaService;
-
-    public Mdai2425Application(PersonaService personaService) {
-        this.personaService = personaService;
-    }
-
+public class Mdai2425Application{
     public static void main(String[] args) {
         SpringApplication.run(Mdai2425Application.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception{
-            String file = "src/main/resources/personas.json";
-            personaService.insertarPersonasDesdeJson(file);
-    }
 
 }
